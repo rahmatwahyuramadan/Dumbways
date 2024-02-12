aeed82bcec02dd7b4b40e0b17b5718d7edb52fda
Rahmat Wahyu <rahmatwahyuramadan@gmail.com>
2024-02-13 06:15:23 +0700

    taskday4

--- asset/css/css/add_project.js
+++ asset/css/css/add_project.js
@@ -1,17 +1,26 @@
 let dataProjects = [];
 
-function dataProject(event) {
+function addProject(event) {
   event.preventDefault();
 
   let Project = document.getElementById("input-project-name").value;
   let startDate = document.getElementById("start-date").value;
   let endDate = document.getElementById("end-date").value;
   let Description = document.getElementById("description").value;
-  let Technologies = document.getElementById("technologies");
+  let inputNodeJS = document.getElementById("inputNodeJS").checked;
+  let inputJavaScript = document.getElementById("inputJavaScript").checked;
+  let inputReactJS = document.getElementById("inputReactJS").checked;
+  let inputTypescript = document.getElementById("inputTypescript").checked;
 
   let dataProject = {
     Project,
-    Description: Description,
+    startDate,
+    endDate,
+    Description,
+    inputNodeJS,
+    inputJavaScript,
+    inputReactJS,
+    inputTypescript
   };
 
   dataProjects.push(dataProject);
@@ -22,24 +31,23 @@ function dataProject(event) {
 }
 
 function renderProject() {
-    document.getElementById("project").innerHTML = "";
+    document.getElementById("addProject").innerHTML = "";
   
     for (let index = 0; index < dataProjects.length; index++) {
-      document.getElementById("project").innerHTML += `
-      <div id="project">
-            <div class="project-list-item">
-                <h1>${dataProjects[index].project}</h1>
-                <p>
-                ${dataProjects[index].description}
-                </p>
-                <img src="asset/css/Gambar/playstore.png" alt="">
-                <img src="asset/css/Gambar/android.png" alt="">
-                <img src="asset/css/Gambar/java.png" alt="">
-                <div class="button">
-                    <button class="buttom-group">Edit</button>
-                    <button class="buttom-group">Delete</button>
-            </div>
+      document.getElementById("addProject").innerHTML += `
+      <div id="addProject">
+      <div class="project-list-item">
+          <h1>${dataProjects[index].Project}</h1>
+          <p class="paragraf">
+          ${dataProjects[index].Description}
+          </p>
+          <img src="asset/css/Gambar/playstore.png" alt="">
+          <img src="asset/css/Gambar/android.png" alt="">
+          <img src="asset/css/Gambar/java.png" alt="">
+          <div class="button">
+              <button class="buttom-group">Edit</button>
+              <button class="buttom-group">Delete</button>
       </div>
-      `;
+      </div>`;
     }
 }
